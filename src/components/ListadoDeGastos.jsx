e64a8fdd6e600e224de4874bb311015e6aeff430
Carlos Ortega <carlosalbertoortegac@.com>
2022-06-14 18:01:50 -0500

    ajuste de usereduce

--- src/components/ListadoDeGastos.jsx
+++ src/components/ListadoDeGastos.jsx
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import MiGasto from './MiGasto';
-import { useState } from 'react';
+
+const initalState = {
+    gastando:''
+}
+
+const reducer = (state , action)=>{
+    return state;
+}
 
 const ListadoDeGastos = ({ gastos}) => {
 
-
-    
+    const [state , dispatch] = useReducer(reducer,initalState)
 
     return (
-        
-        <div className='todolist'>
-           
 
-            <h2>
-                {gastos.length ? "Lista Gastos" : "No hay gastos"}
-            </h2>
-            {gastos.map((gastando, index) => (
-            
-                <>  
-                    <MiGasto
-                        key={index.id}
-                        gastando={gastando} 
-                        
-                    />       
-                </>
-            ))}
-             
-        </div>
+        <>
+            <div>
+                <input type="text"
+                    name='busqueda'
+                    value={state.gastando}
+                    onChange={(e)=>dispatch({type: 'FILTER_TODO'})}
+                />
+                <h2>
+                    {gastos.length ? "Lista Gastos" : "No hay gastos"}
+                </h2>
+                {gastos.map((gastando, index) => (
+                
+                    <>  
+                        <MiGasto
+                            key={index.id}
+                            gastando={gastando} 
+                            
+                        />       
+                    </>
+                ))}
+                
+            </div>
+        
+        </>
+        
+        
          
        
 
